68e4ab9af923b57e62e78baa90c8d6824a15f701
jsweetpotato37 <jsweetpotato37@gmail.com>
2023-12-15 18:08:24 +0900

    sadas

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "home/index.html"),
+        contact: resolve(__dirname, "home/index.html"),
       },
     },
   },
